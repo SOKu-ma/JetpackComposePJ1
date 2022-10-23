0f9c135777f33a7ab9b39796f4dc64b5a13d7bbb
otashuuhei <uverrevu812@gmail.com>
2022-10-24 00:36:46 +0900

    Update

--- app/src/main/java/com/example/jetpackcomposepj1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepj1/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.jetpackcomposepj1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.combinedClickable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepj1.ui.theme.JetpackComposePJ1Theme
@@ -25,30 +31,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
-    var shouldShowOnbording by remember { mutableStateOf(true) }
-    if (shouldShowOnbording) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnbording = false })
+private fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings()
     }
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    Surface() {
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcom to the Basics Codelab!")
+            Text("Welcome to the Basics Codelab!")
             Button(
                 modifier = Modifier.padding(vertical = 24.dp),
                 onClick = onContinueClicked
             ) {
-                Text(text = "Continue")
+                Text("Continue")
             }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(names) { name ->
+            Greeting(name = name)
         }
     }
 }
@@ -62,18 +78,15 @@ fun OnboardingPreview() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("world", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
-    }
-}
-
-@Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded = remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     androidx.compose.material.Surface(
         color = MaterialTheme.colors.primary,
@@ -86,17 +99,30 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4
+                )
+                if (expanded.value) {
+                    Text(text = "TestTest")
+                }
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = {
+                    expanded.value = !expanded.value
+                }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(
+                    if (expanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(id = R.string.show_more)
+                    }
+                )
             }
-
         }
     }
 }
